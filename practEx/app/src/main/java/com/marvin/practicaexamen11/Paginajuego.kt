9988b823c84a9c6009b64abfc579c4041f2bfb0a
Marvin8792 <marvinvazquez893@gmail.com>
2023-10-11 20:38:37 -0600

    Clase

---
+++ practEx/app/src/main/java/com/marvin/practicaexamen11/Paginajuego.kt
@@ -0,0 +1,11 @@
+package com.marvin.practicaexamen11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Paginajuego : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paginajuego)
+    }
+}
