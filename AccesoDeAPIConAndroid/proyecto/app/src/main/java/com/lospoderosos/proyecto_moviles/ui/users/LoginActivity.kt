949e9e4b86fa9f6fc72502e3f8d0a3995dcfa1f7
Marvin8792 <marvinvazquez893@gmail.com>
2023-11-28 16:54:30 -0600

    proyec

--- AccesoDeAPIConAndroid/proyecto/app/src/main/java/com/lospoderosos/proyecto_moviles/ui/users/LoginActivity.kt
+++ AccesoDeAPIConAndroid/proyecto/app/src/main/java/com/lospoderosos/proyecto_moviles/ui/users/LoginActivity.kt
@@ -8,9 +8,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
+import com.lospoderosos.proyecto_moviles.MainActivity
 import com.lospoderosos.proyecto_moviles.R
 import com.lospoderosos.proyecto_moviles.ui.users.SignupActivity
 import java.util.Date
@@ -27,6 +35,8 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var btnRegistrar: TextView
     var db = FirebaseFirestore.getInstance()
 
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -37,6 +47,17 @@ class LoginActivity : AppCompatActivity() {
 
         btnRegistrar.setOnClickListener {
             goToSignup()
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        findViewById<Button>(R.id.btnGoogle).setOnClickListener{
+            googleSignIn();
         }
         buttonLogin.setOnClickListener {
             if(textEmail.text.isNotEmpty() && textPassword.text.isNotEmpty()){
@@ -84,6 +105,47 @@ class LoginActivity : AppCompatActivity() {
                 showAlert("Error","El correo electrónico y contraseña no pueden estar vacíos")
             }
         }
+    }
+
+    private fun googleSignIn() {
+        val signInClient = googleSignInClient.signInIntent
+        launcher.launch(signInClient)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+
+        if (result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+
+            manageResults(task);
+        }
+    }
+
+    private fun manageResults(task: Task<GoogleSignInAccount>) {
+        val account : GoogleSignInAccount? = task .result
+
+        if (account != null) {
+            val credential = GoogleAuthProvider.getCredential(account.idToken, "")
+            auth.signInWithCredential(credential).addOnCompleteListener{
+                if( task.isSuccessful){
+                    //val intent = Intent(this,MainActivity::class.java )
+                    //startActivity(intent)
+                    Intent().let {
+                        setResult(Activity.RESULT_OK)
+                        finish()
+                    }
+
+                    Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        else{
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     private fun goToSignup() {
